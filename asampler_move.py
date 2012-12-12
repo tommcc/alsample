18c9780de06581eabf6f7080760e2d003c1b4f99
Tom McCaffery <tom@tom-mcc>
2012-12-11 23:45:41 -0500

    Create sampler class

--- asampler_move.py
+++ asampler_move.py
@@ -4,16 +4,27 @@ import argparse
 import gzip
 import xml.etree.cElementTree as ET
 
-def open_adv(path):  
+def open_device(path):  
     with gzip.open(path, 'r') as f:
         xml = f.read()
     return ET.fromstring(xml)
 
 def get_sample_refs(xml):
-    return xml.iter('SampleRef')
+    return list(xml.iter('SampleRef'))
+
+class Sample(object):
+    def __init__(self, xml):
+        self.xml = xml
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(description='Manage sample references of Ableton Live device files.')
-    argparser.add_argument('devices', help='Device files (.adg and .adv)')
+    argparser.add_argument('device', nargs='+', help='Device files (.adg and .adv)')
 
     args = argparser.parse_args()
+
+    for devicePath in args.device:
+        device = open_device(devicePath)
+        sample_refs = get_sample_refs(device)
+        for sample_ref in sample_refs:
+            sample = Sample(sample_ref)
+            print(sample)
